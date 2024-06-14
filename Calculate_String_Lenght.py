c09f05d61ebf4fc4a993849b6513c3b1d4fb7004
yigitPy <ygt.developer03py@gmail.com>
2024-06-14 16:33:23 +0300

    Calculate string len added

--- Calculate_String_Lenght.py
+++ Calculate_String_Lenght.py
@@ -1,18 +1,28 @@
+input_str = "yigit ali"
+
+print(len(input_str))
+
+def iterative_len(input_str):
+    
+    count = 0
+    for i in range(len(input_str)):
+        if input_str[i] != ' ':
+            count +=1
+    return count
+
+
+def recursive_len(input_str):
+    if input_str == '':
+        return 0
+    elif input_str[0] == ' ':
+        return recursive_len(input_str[1:])
+    else:
+        return 1 + recursive_len(input_str[1:])
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+print(iterative_len(input_str))
+print(recursive_len(input_str))
 
 
 
