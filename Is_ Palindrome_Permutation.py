c512ba5dc9984b1aaf55a4de6ace050793613cd6
yigitPy <ygt.developer03py@gmail.com>
2024-06-28 17:47:34 +0300

    Is_ Palindrome_Permutation.py have been added

---
+++ Is_ Palindrome_Permutation.py
@@ -0,0 +1,53 @@
+
+def is_polindrome(string):
+    
+    string = string.replace(" "," ")
+    string = string.lower()
+    d = dict()
+    
+    for i in string:
+        if i in d:
+            d[i] += 1
+        else:
+            d[i] = 1
+     
+    odd_count = 0        
+    for k, v in d.items():
+        if v % 2 != 0:
+            odd_count += 1
+            if odd_count > 1:
+                return False
+    return True
+
+
+palin_perm = "Tact Coa"
+not_palin_perm = "This is not a palindrome permutation"
+
+print(is_palin_perm(palin_perm))  
+print(is_palin_perm(not_palin_perm))  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
